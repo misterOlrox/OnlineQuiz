170ec3fc9dc4ebbc97b8c41a14f464cad8ac1697
MisterOlrox <nikitakholdobin@yandex.ru>
2020-04-25 22:44:40 +0300

    Beans replacement

--- src/main/java/com/olrox/quiz/config/BeansConfig.java
+++ src/main/java/com/olrox/quiz/config/BeansConfig.java
@@ -3,6 +3,8 @@ package com.olrox.quiz.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Clock;
@@ -10,6 +12,12 @@ import java.time.Clock;
 @Configuration
 @EnableScheduling
 public class BeansConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
--- src/main/java/com/olrox/quiz/config/WebSecurityConfig.java
+++ src/main/java/com/olrox/quiz/config/WebSecurityConfig.java
@@ -3,36 +3,21 @@ package com.olrox.quiz.config;
 import com.olrox.quiz.entity.Role;
 import com.olrox.quiz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final DataSource dataSource;
     private UserService userService;
     private PasswordEncoder passwordEncoder;
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder(8);
-    }
-
-    @Autowired
-    public WebSecurityConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     @Autowired
     public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
